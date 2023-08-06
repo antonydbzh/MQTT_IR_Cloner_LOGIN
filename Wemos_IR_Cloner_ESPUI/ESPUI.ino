65bfff7f7a1a3c7f36dda59a6e3348c09f040132
Antonydbzh <aauffret@lespetitsdebrouillards.org>
2023-08-06 15:18:30 +0200

    Update ESPUI.ino

--- Wemos_IR_Cloner_ESPUI/ESPUI.ino
+++ Wemos_IR_Cloner_ESPUI/ESPUI.ino
@@ -23,6 +23,11 @@ void espui_init() {
   ESPUI.addControl(Max, "", "32", None, wifi_ssid_text);
   ESPUI.addControl(Max, "", "64", None, wifi_pass_text);
   mqtt_server_text = ESPUI.addControl(Text, "MQTT server", stored_mqtt_server, Alizarin, wifitab, textCallback);
+  
+  // Ici j'ajoute la gestion du User et Mdp MQTT
+  mqtt_user_text = ESPUI.addControl(Text, "MQTT user", stored_mqtt_user, Alizarin, wifitab, textCallback);
+  mqtt_pass_text = ESPUI.addControl(Text, "MQTT password", stored_mqtt_pass, Alizarin, wifitab, textCallback);
+  ESPUI.setInputType(mqtt_pass_text, "password");
   mqtt_topic_in_text = ESPUI.addControl(Text, "MQTT topic IN", stored_mqtt_topic_in, Alizarin, wifitab, textCallback);
   mqtt_topic_out_text = ESPUI.addControl(Text, "MQTT topic OUT", stored_mqtt_topic_out, Alizarin, wifitab, textCallback);
   mqtt_enabled_switch = ESPUI.addControl(Switcher, "Enable MQTT", String(mqtt_enabled), Alizarin, wifitab, textCallback);
@@ -33,6 +38,8 @@ void espui_init() {
   ESPUI.setEnabled(wifi_ssid_text, true);
   ESPUI.setEnabled(wifi_pass_text, true);
   ESPUI.setEnabled(mqtt_server_text, true);
+  ESPUI.setEnabled(mqtt_user_text, true); // le user MQTT
+  ESPUI.setEnabled(mqtt_pass_text, true); // le mdp MQTT
   ESPUI.setEnabled(mqtt_topic_in_text, true);
   ESPUI.setEnabled(mqtt_topic_out_text, true);
   ESPUI.setEnabled(mqtt_enabled_switch, true);
